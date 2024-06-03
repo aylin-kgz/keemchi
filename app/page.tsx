0fa65a93308cddbab6352777c767d3d68ed3854e
nomad-alt <nomad-alt@proton.me>
2024-06-03 18:56:40 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,154 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center justify-between font-poppins">
+      <div></div>
+      <nav className="w-full flex items-center justify-between lg:flex p-4">
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <div className="">
+          <ul
+            className={`md:flex ${
+              isOpen ? "block" : "hidden"
+            } text-white space-x-6`}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <li>
+              <a href="#home" className="hover:underline">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#restaurants" className="hover:underline">
+                Our Restaurants
+              </a>
+            </li>
+            <li>
+              <a href="#menu" className="hover:underline">
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="hover:underline">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="lg:hidden md:hidden"
+        >
+          <FiMenu className="h-8 w-8 text-white" />
+        </button>
+      </nav>
+      <div className="h-screen flex justify-center items-center p-4">
+        <div className="relative z-10 flex flex-col items-center text-center space-y-10 opacity-85 mb-20">
+          <Image
+            src="/keemchi.png"
+            alt="Keemchi Logo"
+            width={300}
+            height={300}
+          />
+          <p className="text-white font-light">MODERN KOREAN RESTAURANT</p>
+          <p className="text-white font-thin lg:w-2/3 md:w-2/3">
+            Explore a fusion of traditional Korean flavors and modern techniques
+            in Stockholm and Uppsala
+          </p>
+          <button className="bg-cream py-2 px-4 rounded opacity-90 hover:opacity-100 w-1/2 lg:w-1/4 md:w-1/4">
+            <a href="#menu" className="text-blue-900 font-semibold">
+              Menu
+            </a>
+          </button>
         </div>
       </div>
+      <div></div>
+      <footer className="bg-cream text-blue-900 p-10 w-full flex flex-col md:flex-row lg:flex-row justify-around items-start space-y-6 md:space-y-0 lg:space-y-0">
+        <div className="flex flex-col items-start space-y-2">
+          <Image
+            src="/keemchi-blue.png"
+            alt="Keemchi Logo"
+            width={100}
+            height={100}
+          />
+          <p>Modern Korean Restaurant</p>
+          <Image
+            src="/Logo-blue.png"
+            alt="Keemchi Logo"
+            width={30}
+            height={30}
+          />
+        </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
+        <div className="space-y-4">
+          <p className="font-bold">QUICK MENU</p>
+          <ul className="space-y-2">
+            <li>
+              <a href="#home" className="hover:underline">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#restaurants" className="hover:underline">
+                Our Restaurants
+              </a>
+            </li>
+            <li>
+              <a href="#menu" className="hover:underline">
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="hover:underline">
+                About Us
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold">CONTACT</p>
+          <p>Phone: +46 (0)704 59 02 99</p>
+          <p>
+            Email:{" "}
+            <a href="mailto:info@keemchi.se" className="hover:underline">
+              info@keemchi.se
+            </a>
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        </div>
+        <div className="space-y-2">
+          <p className="font-bold">SOCIAL</p>
+          <div className="flex justify-start space-x-4">
+            <a
+              href="https://instagram.com"
+              className="text-blue-900 hover:underline"
+            >
+              <FaInstagram size={24} />
+            </a>
+            <a
+              href="https://tiktok.com"
+              className="text-blue-900 hover:underline"
+            >
+              <FaTiktok size={24} />
+            </a>
+          </div>
+        </div>
+      </footer>
+      <div className="bg-cream w-full py-2">
+        <hr className="border-t border-blue-900" />
+        <p className="text-center mt-2 text-xs">
+          © {new Date().getFullYear()} Keemchi. All rights reserved.
+        </p>
       </div>
     </main>
   );
